36d22cf7271ff830a552ae6841d574a2de85f7b1
okpiri <okpiri@gmail.com>
2025-05-06 15:19:11 +0900

    port 변경

--- reactapp2.client/vite.config.js
+++ reactapp2.client/vite.config.js
@@ -35,7 +35,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7223';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5201';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -60,7 +60,7 @@ export default defineConfig({
                 secure: false
             }
         },
-        port: parseInt(env.DEV_SERVER_PORT || '13392'),
+        port: parseInt(env.DEV_SERVER_PORT || '5101'),
         //https: {
         //    key: fs.readFileSync(keyFilePath),
         //    cert: fs.readFileSync(certFilePath),
